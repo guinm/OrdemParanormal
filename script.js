2665e177df835b2bea79184147adfcd1c798afdf
JustG <guimachado94@gmail.com>
2022-10-27 15:18:09 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ $('ul li').on('click', function() {
 	$('li').removeClass('active');
 	$(this).addClass('active');
 });
+
+function myFunction() {
+  var element = document.getElementById("menu");
+  element.classList.toggle("open");
+}
